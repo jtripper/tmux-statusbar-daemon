2799499ecdcd2ac0a2a28feed3bda929174f292e
jtripper <jack@jtripper.net>
2013-12-16 00:31:36 -0500

    updated message script

--- plugins/OutputMessages.rb
+++ plugins/OutputMessages.rb
@@ -3,8 +3,12 @@ require_relative '../lib/statusbar.rb'
 # PM notifications
 class OutputMessages < StatusBarPlugin
   def run
-    msg = File.open("#{ENV["HOME"]}/.messages").gets.rstrip
-    if msg != ""
+    begin
+      msg = File.open("#{ENV["HOME"]}/.messages").gets.rstrip
+    rescue
+    end
+
+    if msg and  msg != ""
       @output = "#{green}PM from: #{yellow}#{msg}#{blue}"
       File.open("#{ENV["HOME"]}/.messages", "w").close
     else
