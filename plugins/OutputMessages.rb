b0adac20114f1846b6279f45f06131b088a58f8e
jtripper <jack@jtripper.net>
2013-12-16 09:50:08 -0500

    updated message outputter

--- plugins/OutputMessages.rb
+++ plugins/OutputMessages.rb
@@ -3,14 +3,16 @@ require_relative '../lib/statusbar.rb'
 # PM notifications
 class OutputMessages < StatusBarPlugin
   def run
+    return if not File.exists? "#{ENV["HOME"]}/.irssi"
+
     begin
-      msg = File.open("#{ENV["HOME"]}/.messages").gets.rstrip
+      msg = File.open("#{ENV["HOME"]}/.irssi/messages").gets.rstrip
     rescue
     end
 
     if msg and  msg != ""
       @output = "#{green}PM from: #{yellow}#{msg}#{blue}"
-      File.open("#{ENV["HOME"]}/.messages", "w").close
+      File.open("#{ENV["HOME"]}/.irssi/messages", "w").close
     else
       @output = "#{green}No new messages#{blue}"
     end
